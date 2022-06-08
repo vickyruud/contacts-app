aa7ad4bc7f3326a2ba929630c05d1eac16820aa1
vickyruud <aprajit.k@gmail.com>
2022-06-08 08:36:55 -0400

    set up server and placeholder routes

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/contacts', (req, res, next) => {
+  // get placeholder
+});
+
+router.post('/contacts', (req, res, next) => {
+  // post placeholder
+});
+
+router.delete('/contacts/:id', (req, res, next) => {
+  // delete placeholder
+});
+
+module.exports = router;
